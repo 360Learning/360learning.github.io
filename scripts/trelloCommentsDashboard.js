cbabe452e5cb4afde218e1f3084b864cb87015d6
Colin Grodecoeur <colin.grodecoeur@360learning.com>
2022-04-16 09:35:49 +0100

    Fetch the comments and display them in a table
    With some basic error management

--- scripts/trelloCommentsDashboard.js
+++ scripts/trelloCommentsDashboard.js
@@ -1,9 +1,11 @@
+const TRELLO_BASE_URL = "https://trello.com";
 const TRELLO_CREDENTIALS_HELPER_FILE = "https://docs.google.com/document/d/1HwaedNa861gkj93TaradW5n0MID8cbeamiU5SXCvX64/edit#heading=h.ijjo2dol5z6v"
 
 new Vue({
     el: '#app',
     data: {
         comments: null,
+        error: null,
         trelloCredentialsHelperFile: TRELLO_CREDENTIALS_HELPER_FILE,
         credentials: {
             trelloApiKey: "",
@@ -18,7 +20,33 @@ new Vue({
     },
     methods: {
         async fetchComments() {
-            this.comments = [];
+            this.comments = null;
+            this.error = null;
+            try {
+                const comments = await fetchUserComments(this.username, {}, this.credentials);
+                this.comments = parseComments(comments);
+            } catch (error) {
+                this.error = error.message;
+            }
         }
     }
 });
+
+function parseComments(comments) {
+    return comments
+        .map(parseComment);
+
+    function parseComment(comment) {
+        return {
+            board: comment.data.board.name,
+            card: comment.data.card.name,
+            date: comment.date.slice(0, 10),
+            text: comment.data.text,
+            link: buildLinkToComment(comment)
+        };
+    }
+}
+
+function buildLinkToComment(comment) {
+    return `${TRELLO_BASE_URL}/c/${comment.data.card.shortLink}#action-${comment.id}`;
+}
